0eda06f1950a6d81fe13d6f1a99289a397ba8e84
Vishnu Vardhan Reddy 2 <vred19@sapient.com>
2018-01-23 12:44:26 +0530

    updated the query limit

--- src/routes/contactroute.js
+++ src/routes/contactroute.js
@@ -14,7 +14,7 @@ module.exports = function(dbcon) {
                 if (err) console.log(err)
 
                 client = client;
-                var query = format('SELECT * FROM salesforce.contact ORDER BY id ASC Limit 10');
+                var query = format('SELECT * FROM salesforce.contact ORDER BY id ASC Limit 100');
                 client.query(query, function(err, result) {
                     if (err) {
                         console.log("not able to get connection " + err);
